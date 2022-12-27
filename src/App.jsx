2d075f1c98156323f3e7b7ee20a2537bef2d3aca
Esteban <estebanlongo274@gmail.com>
2022-12-27 17:57:03 -0300

    add landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import Home from "./components/Home/Home.jsx";
 import Info from "./components/Informacion/Info.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Faqs from "./components/Faqs/Faqs.jsx";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route index element={<Home />} />
         <Route path="/" element={<NavBar />} />
+        <Route index element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/info" element={<Info />} />
         <Route path="/faqs" element={<Faqs />} />
       </Routes>
